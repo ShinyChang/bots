7ddc8bca1d19a3051c81f1f12a5380ea1b28b770
Shiny <contact@shinychang.net>
2017-05-12 15:59:15 +0800

    Remove unused file

--- firebase.js
+++
@@ -1,7 +0,0 @@
-const user = require('./services/user')
-setTimeout(() => {
-  const userId = user.getUserIdByServiceId('github', 'ShinyChang')
-  const jiraUserId = user.getServiceId(userId, 'jira')
-  console.log(jiraUserId)
-}, 3000)
-
