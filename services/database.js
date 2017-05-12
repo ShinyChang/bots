d1bc13f7737656d2ba04a54bd9be35cccbaf5ebd
Shiny <contact@shinychang.net>
2017-05-12 18:22:55 +0800

    Fix firebase bug

--- services/database.js
+++ services/database.js
@@ -1,10 +1,14 @@
 const admin = require("firebase-admin")
 
+// http://stackoverflow.com/questions/41287108/deploying-firebase-app-with-service-account-to-heroku-environment-variables-wit
+const privateKey = process.env.NODE_ENV === 'production' 
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        : process.env.FIREBASE_PRIVATE_KEY
 const app = admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
+    privateKey
   }),
   databaseURL: process.env.FIREBASE_DATABASE_URL
 })
