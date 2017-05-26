e99b7352f5084db4820e156f425cea30b1e0bc28
Shiny <contact@shinychang.net>
2017-05-26 12:07:00 +0800

    Reduce interval time of GitHubActivityToSlack

--- corn/index.js
+++ corn/index.js
@@ -6,9 +6,9 @@ const HOUR = 60 * MIN
 const DAY = 24 * HOUR
 
 const tasks = [
-  {interval: 30 * SEC, job: require('./syncGitHubToJira')},
+  {interval: 30 * SEC, job: require('./syncGitHubToJira')}, // 6 api calls, 12/min, 720/hr
   {interval: 1 * MIN, job: require('./pingHonestbeeProd')},
-  {interval: 30 * SEC, job: require('./GitHubActivityToSlack')},
+  {interval: 5 * SEC, job: require('./GitHubActivityToSlack')}, // 1 api call, 12/min, 720/hr
   {interval: 30 * SEC, job: require('./syncTravisCIToJira')},
 ]
 
