b73887e29eb40e6dd37970c38a57d2200c86af63
Shiny <contact@shinychang.net>
2017-11-01 14:27:12 +0800

    Add response

--- skills/lokalise/index.js
+++ skills/lokalise/index.js
@@ -20,7 +20,7 @@ const handler = ([action, ...rest], actorId) => {
 
   switch (action) {
     case 'release':
-      return lokalise.handler(rest)
+      return lokalise.handler(rest).then(() => return 'released')
     default:
       return Promise.resolve(`usage:
 lokalise release`)
