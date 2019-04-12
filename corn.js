58813a4ba434242a584e3faaaea9d6186ed72a2e
Shiny <contact@shinychang.net>
2019-04-12 16:54:27 +0800

    Update corn.js

--- corn.js
+++ corn.js
@@ -1,16 +1,8 @@
-require('dotenv').config();
-const https = require('https');
+require("dotenv").config();
+const https = require("https");
 
 const baseUrl = process.env.AUDIT_URL;
 
-const urls = [
-  'https://www.honestbee.tw/',
-  'https://www.honestbee.tw/zh-TW/groceries',
-  'https://www.honestbee.tw/zh-TW/groceries/stores/american-wholesaler',
-  'https://www.honestbee.tw/zh-TW/groceries/stores/american-wholesaler/products/2186868',
-  'https://deliveroo.com.sg/',
-  'https://deliveroo.com.sg/restaurants/singapore/tiong-bahru?postcode=159919',
-  'https://deliveroo.com.sg/menu/singapore/orchard/5-star-hainanese-chicken-rice-river-valley-road?day=today&postcode=159919&time=ASAP'
-];
+const urls = ["https://www.honestbee.tw/"];
 
 urls.map(url => https.get(`${baseUrl}?url=${url}`));
